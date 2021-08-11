48bd0217bd64ea844cc893a481d9ef10675e9859
João Pedro Carvalho <jppcarvalho8@gmail.com>
2021-08-11 12:55:13 -0300

    criando a classe de conexão

--- src/login_register_design/My_CNX.java
+++ src/login_register_design/My_CNX.java
@@ -1,22 +1,37 @@
 package login_register_design;
 
-import com.sun.jdi.connect.spi.Connection;
-import com.mysql.cj.jdbc;
+import com.mysql.cj.jdbc.MysqlDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class My_CNX {
     
-    private String servername  = "localhost";
-    private String username    = "root";
-    private String senha       = "";
-    private String dbname      = " users_db";
-    private Integer portnumber = 3306;
+    private static String servername  = "localhost";
+    private static String username    = "root";
+    private static String senha       = "";
+    private static String dbname      = " users_db";
+    private static Integer portnumber = 3306;
     
     public static Connection getConnection () 
     {
         Connection cnx = null;
         
-        MysqlDatasource datasource = new MysqlDatasource();
+        MysqlDataSource datasource = new MysqlDataSource();
+        
+        datasource.setServerName(servername);
+        datasource.setUser(username);
+        datasource.setPassword(senha);
+        datasource.setDatabaseName(dbname);
+        datasource.setPortNumber(portnumber);
+        
+        try {
+            cnx = datasource.getConnection();
+        } catch (SQLException ex) {
+            Logger.getLogger(" Obter Conexão ---> " + My_CNX.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
         return cnx;
     }
